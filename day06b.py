325d79ca846760fdf4d31d91359c353e19bb1fdd
Gyaha <gyaha@users.noreply.github.com>
2021-07-13 17:54:04 +0200

    day 6 done

---
+++ day06b.py
@@ -0,0 +1,49 @@
+SIZE = 1000
+
+
+def light_matrix(s: str) -> int:
+    m = [[0 for i in range(SIZE)] for i in range(SIZE)]
+    for l in s.splitlines():
+        light_instruction(m, l)
+    t = 0
+    for a in m:
+        t += sum(a)
+    return t
+
+
+def light_instruction(m: list, l: str):
+    sl = l.split()
+    if sl[0] == "turn":
+        sx, sy = sl[2].split(",")
+        ex, ey = sl[4].split(",")
+
+        light_turn(m, sl[1] == "on", int(sx), int(sy), int(ex), int(ey))
+    else:
+        sx, sy = sl[1].split(",")
+        ex, ey = sl[3].split(",")
+
+        light_toggle(m, int(sx), int(sy), int(ex), int(ey))
+
+
+def light_turn(m: list, on: bool, sx, sy, ex, ey):
+    print(on, sx, sy, ex, ey)
+    for y in range(sy - 1, ey):
+        for x in range(sx - 1, ex):
+            if on:
+                m[y][x] += 1
+            else:
+                m[y][x] -= 1
+                if m[y][x] < 0:
+                    m[y][x] = 0
+
+
+def light_toggle(m: list, sx, sy, ex, ey):
+    print(sx, sy, ex, ey)
+    for y in range(sy - 1, ey):
+        for x in range(sx - 1, ex):
+            m[y][x] += 2
+
+
+with open("input06") as file:
+    data = file.read()
+    print(light_matrix(data))
